4e89826514fd94fef957427866a075fabc67b625
Wahyu Fauzi <my.uncle.wahyu@gmail.com>
2023-11-30 18:12:41 +0800

    added java

---
+++ E. LOGICAL TEST/SoalA.java
@@ -0,0 +1,26 @@
+
+public class SoalA  {
+
+  public static void main(String[] args) {
+    int n = 5; // Replace with desired number of rows
+    addedStar(n);
+}
+
+public static void addedStar(int n) {
+    StringBuilder output = new StringBuilder(); // Use StringBuilder for efficient string concatenation
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= n + 2; j++) {
+            if (j == i || j == i + 1) {
+                output.append('*');
+            } else {
+                output.append(j + 1);
+            }
+        }
+        output.append('\n'); // Add a new line after each row
+    }
+
+    System.out.println(output.toString());
+}
+
+}
---
+++ E. LOGICAL TEST/SoalB.java
@@ -0,0 +1,19 @@
+public class SoalB {
+  public static int maximumDifference(int[] nums) {
+    int maxDifference = 0;
+    int min = nums[0];
+
+    for (int num : nums) {
+        min = Math.min(min, num);
+        maxDifference = Math.max(maxDifference, num - min);
+    }
+
+    return maxDifference == 0 ? -1 : maxDifference;
+  }
+
+  public static void main(String[] args) {
+      int[] nums = {7, 1, 5, 3};
+      int maxDifference = maximumDifference(nums);
+      System.out.println(maxDifference);
+  }
+}
